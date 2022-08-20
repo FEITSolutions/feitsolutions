8096a0a264e24f1a4176e704c505c0cb1953cc78
Patrick Fay <patrick.fay@live.com>
2022-08-20 07:36:59 -0400

    Added the UsersController class and made the endpoints asynchronous.

---
+++ Create dbo.Users.sql
@@ -0,0 +1,14 @@
+﻿USE [feitsolutionsDB]
+GO
+
+/****** Object: Table [dbo].[Users] Script Date: 8/19/2022 11:48:37 PM ******/
+SET IDENTITY_INSERT [dbo].[Users] ON;
+
+INSERT INTO [dbo].[Users]([Id], [UserName])
+VALUES(1, 'Bob');
+
+INSERT INTO [dbo].[Users]([Id], [UserName])
+VALUES(2, 'Tom');
+
+
+
---
+++ Create dbo.Users_1.sql
@@ -0,0 +1,13 @@
+﻿USE [feitsolutionsDB]
+SET IDENTITY_INSERT [dbo].[Users] ON;
+
+INSERT INTO [dbo].[Users]([Id], [UserName])
+VALUES(1, 'Bob');
+
+INSERT INTO [dbo].[Users]([Id], [UserName])
+VALUES(2, 'Tom');
+
+GO
+
+
+
