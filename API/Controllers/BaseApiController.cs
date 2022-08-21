e79b4ae6d9277f870d681f8e21e1906dc3329bcc
Patrick Fay <patrick.fay@live.com>
2022-08-21 05:37:30 -0400

    Update the user entity to include authentication, Create a base API controller

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,11 +2,10 @@
 
 namespace API.Controllers
 {
-    public class BaseApiController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+       
     }
 }
